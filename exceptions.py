b1fa6c0e81d0c9df1f63baa0c6f66e8858e835b5
9098787 <obmen79obmen@gmail.com>
2024-04-07 15:11:09 +0200

    Added change_password method
    And some fixes

--- exceptions.py
+++ exceptions.py
@@ -38,3 +38,29 @@ class GetMessagesError(Exception):
             return self.message
         else:
             return 'GetMessagesError'
+        
+class NeedLogin(Exception):
+    def __init__(self, *args) -> None:
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+    
+    def __str__(self) -> str:
+        if self.message:
+            return self.message
+        else:
+            return 'NeedLogin'
+        
+class ChangePasswordError(Exception):
+    def __init__(self, *args) -> None:
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+    
+    def __str__(self) -> str:
+        if self.message:
+            return self.message
+        else:
+            return 'ChangePasswordError'
--- firstmail.py
+++ firstmail.py
@@ -1,9 +1,11 @@
 from requests import Session
 from typing import Literal
+from string import ascii_letters
 import base64
 import pyotp
+import random
 
-from exceptions import LoginError, ExpiredJWT, GetMessagesError
+from exceptions import LoginError, ExpiredJWT, GetMessagesError, NeedLogin, ChangePasswordError
 from endpoints import endpoints
 
 class Firstmail:
@@ -18,11 +20,16 @@ class Firstmail:
         # Creating session
         self.__session = Session()
         self.__session.headers = {
-            'User-Agent': user_agent
+            'User-Agent': user_agent,
+            'Authorization': None
         }
         self.__session.proxies = proxies
+        self.__email = None
+        self.__password = None
+        self.__otp_key = None
     
-    def __getotp(self, otpcode: str) -> str:
+    @staticmethod
+    def getotp(otpcode: str) -> str:
         '''
         Handle "otpcode" from login method's parameter
         '''
@@ -35,6 +42,30 @@ class Firstmail:
                 return current_code
             except base64.binascii.Error:
                 return otpcode
+
+    @staticmethod
+    def validate_password(password: str) -> bool:
+        '''
+        Validating password before change
+        '''
+        if len(password) < 8:
+            return False
+        for char in password:
+            if char not in ascii_letters + '0123456789' + '!@#$%^&*()':
+                return False
+        return True
+
+    @staticmethod
+    def generate_password() -> str:
+        '''
+        :returns: random password (8 chars + 4 digits)
+        '''
+        password = ''
+        for _ in range(8):
+            password += random.choice(ascii_letters)
+        for _ in range(4):
+            password += str(random.randint(0, 9))
+        return password
 
     def check_jwt(self) -> bool:
         '''
@@ -78,16 +109,21 @@ class Firstmail:
         # Saving JWT
         jwt_token = login_result['jwtToken']
         self.__session.headers['Authorization'] = 'Bearer ' + jwt_token
+        self.__email = email
+        self.__password = password
 
-    def get_messages(self, folder: Literal['inbox', 'starred', 'spam'] = 'inbox') -> dict[str: int, str: list]:
+    def get_messages(self, folder: Literal['inbox', 'starred', 'spam'] = 'inbox') -> dict:
         '''
-        Get messages method
+        Get messages method. This method returns only first 10 messages from folder.
+        If you need more, you can use search method
 
         :param folder: inbox/starred/spam, default inbox
         :returns: dictionary with count and list of messages
         '''
         if folder not in ['inbox', 'starred', 'spam']:
             raise GetMessagesError('Invalid "folder" parameter')
+        if 'Authorization' not in self.__session.headers:
+            raise NeedLogin('Need login for using this method')
 
         payload = {
             'folder': folder
@@ -98,18 +134,67 @@ class Firstmail:
         get_messages_result = get_messages_request.json()
         return get_messages_result
     
-    def change_password(new_password: str) -> None:
+    def change_password(self, new_password: str) -> None:
         '''
         Change password method
 
         :param new_password: new password, 8 only ASCII-letters with special chars
         '''
+        # Exceptions before request
+        if 'Authorization' not in self.__session.headers:
+            raise NeedLogin('Need login for using this method')
+        if not self.validate_password(new_password):
+            raise ChangePasswordError('The password does not meet the requirements')
+        # Main request
+        payload = {
+            'cpassword': self.__password,
+            'npassword': new_password,
+            'npassword2': new_password
+        }
+        change_password_request = self.__session.post(endpoints['change_password'], data=payload)
+        # Catching errors
+        if change_password_request.status_code in [403, 500]:
+            raise ExpiredJWT('Token has expired')
+        change_password_result = change_password_request.json()
+        if change_password_result['error']:
+            raise ChangePasswordError(change_password_result['message'])
+        # Saving new password and JWT
+        self.__password = new_password
+        self.__session.headers['Authorization'] = change_password_result['Token']
+
+    def search(self, query: str = '') -> dict:
+        '''
+        Search messages method
+        Only with this method you can get all messages from firstmail without using IMAP
+
+        :param query: search query (you can keep it empty to get all messages)
+        :returns: dict with messages count, empty (True if empty, else False), messages list
+        '''
+        # Exceptions before request
+        if 'Authorization' not in self.__session.headers:
+            raise NeedLogin('Need login for using this method')
+        
+
+    def get_credentials(self) -> dict:
+        '''
+        Get credentials method
+
+        :returns: dict with email, password, JWT and 2fa secret key
+        '''
+        return {
+            'email': self.__email,
+            'password': self.__password,
+            'jwt_token': self.__session.headers['Authorization'],
+            'otp_key': self.__otp_key
+        }
+
 
 
 
 
 
 fm = Firstmail()
-fm.login('charlesortega2014@semiaemail.ru', 'xkpkddtj8497')
+fm.login('hvdxukwd@sfirstmail.com', 'Ganster09')
 messages = fm.get_messages()
 print(messages)
+print(fm.get_credentials())
